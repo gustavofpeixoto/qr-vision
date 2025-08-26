1016e1902a197a2bb525acd790eb3abbfb9ca0ca
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-08-26 08:11:52 -0300

    Adicionar repositório de análises de vídeo.

---
+++ src/QrVision.Infra/Repositories/VideoAnalysisRepository.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson;
+using QrVision.Domain.Entities;
+using QrVision.Domain.Interfaces;
+using QrVision.Infra.Settings;
+using MongoDB.Driver;
+
+namespace QrVision.Infra.Repositories
+{
+    public class VideoAnalysisRepository : IVideoAnalysisRepository
+    {
+        private readonly IMongoCollection<VideoAnalysis> _videoAnalysisCollection;
+
+        static VideoAnalysisRepository()
+        {
+            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
+            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            var conventionPack = new ConventionPack { new CamelCaseElementNameConvention() };
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+
+            BsonClassMap.RegisterClassMap<VideoAnalysis>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+                cm.MapIdProperty(p => p.Id);
+            });
+
+            BsonClassMap.RegisterClassMap<QrCodeResult>(cm =>
+            {
+                cm.AutoMap();
+                cm.SetIgnoreExtraElements(true);
+            });
+        }
+
+        public VideoAnalysisRepository(MongoDbSettings mongoDbSettings)
+        {
+            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
+            var mongoDatabase = mongoClient.GetDatabase(mongoDbSettings.DatabaseName);
+
+            _videoAnalysisCollection = mongoDatabase.GetCollection<VideoAnalysis>("video-analysis");
+        }
+
+        public Task AddAsync(VideoAnalysis videoAnalysis) => _videoAnalysisCollection.InsertOneAsync(videoAnalysis);
+
+        public async Task GetByIdAsync(Guid id)
+            => await _videoAnalysisCollection.Find(va => va.Id == id).SingleOrDefaultAsync();
+
+        public async Task UpdateAsync(VideoAnalysis videoAnalysis)
+            => await _videoAnalysisCollection.ReplaceOneAsync(va => va.Id == videoAnalysis.Id, videoAnalysis);
+    }
+}
