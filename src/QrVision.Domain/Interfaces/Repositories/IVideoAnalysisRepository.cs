65bdc8a9a0ed48212cf5f9f1deddfd4cb224773d
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-08-26 08:51:59 -0300

    Mover IVideoAnalysisRepository para o namespace Interfaces.Repositories

---
+++ src/QrVision.Domain/Interfaces/Repositories/IVideoAnalysisRepository.cs
@@ -0,0 +1,11 @@
+﻿using QrVision.Domain.Entities;
+
+namespace QrVision.Domain.Interfaces.Repositories
+{
+    public interface IVideoAnalysisRepository
+    {
+        Task AddAsync(VideoAnalysis videoAnalysis);
+        Task GetByIdAsync(Guid id);
+        Task UpdateAsync(VideoAnalysis videoAnalysis);
+    }
+}
