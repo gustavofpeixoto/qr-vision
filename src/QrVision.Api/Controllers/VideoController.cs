fa436f7a2b8b5650cc299fc410ca457da4d5ac67
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-08-29 09:05:12 -0300

    Adaptar controller de vídeo para processar vídeos de forma sincrona. Apenas para fins de teste.

--- src/QrVision.Api/Controllers/VideoController.cs
+++ src/QrVision.Api/Controllers/VideoController.cs
@@ -1,14 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QrVision.Domain.Interfaces.Services;
 
 namespace QrVision.Api.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class VideoController : ControllerBase
+    public class VideoController(IVideoAnalysisService videoAnalysisService) : ControllerBase
     {
         [HttpPost("/upload")]
         public async Task<IActionResult> UploadAsync(IFormFile videoFile)
         {
+            if (videoFile == null || videoFile.Length == 0)
+                return BadRequest("Nenhum arquivo enviado.");
+
+            // 1. Salvar o arquivo temporariamente
+            var uploadsPath = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+            if (!Directory.Exists(uploadsPath)) Directory.CreateDirectory(uploadsPath);
+
+            var fileName = $"{Guid.NewGuid()}{Path.GetExtension(videoFile.FileName)}";
+            var filePath = Path.Combine(uploadsPath, fileName);
+
+            await using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await videoFile.CopyToAsync(stream);
+            }
+
+            var result = await videoAnalysisService.ExtractAndDecodeQrCodeAsync(filePath, new CancellationToken());
+
             return Accepted();
         }
     }
