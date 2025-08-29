768e210d41520c3215d48a91504d97fba6a5007a
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-08-29 14:17:32 -0300

    Incluir classe para facilitar as configurações globais do FFmpeg

---
+++ src/QrVision.Domain/FfmpegSettings.cs
@@ -0,0 +1,12 @@
+﻿using FFMpegCore;
+
+namespace QrVision.Domain
+{
+    public static class FfmpegSettings
+    {
+        public static void AddFfmpegGlobalSettings()
+        {
+            GlobalFFOptions.Configure(options => options.BinaryFolder = "./FFmpeg");
+        }
+    }
+}
