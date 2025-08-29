1c2f585d2ab920cd2975b8adec1672bb97cfb96a
Gustavo Peixoto <gustavo.fonseca.peixoto@gmail.com>
2025-08-29 14:16:48 -0300

    Incluir método para facilitar configurações de serialização de deserialização

---
+++ src/QrVision.Domain/Serialization/JsonSerializerHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+
+namespace QrVision.Domain.Serialization
+{
+    public static class JsonSerializerHelper
+    {
+        private static readonly JsonSerializerOptions _options = new()
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+            PropertyNameCaseInsensitive = true,
+            ReferenceHandler = ReferenceHandler.IgnoreCycles,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+        };
+
+        public static string Serialize<T>(T value) => JsonSerializer.Serialize(value, _options);
+        public static T Deserialize<T>(string json) => JsonSerializer.Deserialize<T>(json, _options);
+    }
+}
